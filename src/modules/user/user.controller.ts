b2943203e530cc0f1899d5a1bfe7b18e98944fab
Amitsen11 <89931804+Amitsen11@users.noreply.github.com>
2023-11-24 23:54:14 +0600

    done controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -5,6 +5,38 @@ const createUser = async (req: Request, res: Response) => {
   try {
     const { user: userData } = req.body;
     const result = await userService.createNewUserIntoDB(userData);
+    res.status(200).json({
+      success: true,
+      message: 'User  Created Successfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getAllUser = async (req: Request, res: Response) => {
+  try {
+    const result = await userService.getAllUserFromAllDB();
+    res.status(200).json({
+      success: true,
+      message: 'User are retrieved Successfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getSingleUser = async (req: Request, res: Response) => {
+  try {
+    const { studentId } = req.params;
+    const result = await userService.getSingleUserFromAllDB(studentId);
+    res.status(200).json({
+      success: true,
+      message: 'Student is retrieved Successfully',
+      data: result,
+    });
   } catch (err) {
     console.log(err);
   }
